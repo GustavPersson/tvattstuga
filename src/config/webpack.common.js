3a84b09c20d0d992461f2947208c53d7c855e2f9
Gustav Persson <gustav.persson@modcam.io>
2016-11-15 22:42:52 +0100

    Koa server and Webpack 2

--- src/config/webpack.common.js
+++ src/config/webpack.common.js
@@ -5,6 +5,7 @@ const { SRC, DIST } = require('./paths');
 
 module.exports = {
   devtool: 'source-map',
+  context: SRC,
   output: {
     path: DIST,
     filename: '[name].js',
@@ -12,55 +13,56 @@ module.exports = {
   },
   plugins: [
     new ExtractTextPlugin('[name].css'),
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
-        PORT: JSON.stringify(process.env.PORT),
-        DEBUG: JSON.stringify(process.env.DEBUG),
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      'process.env.PORT': JSON.stringify(process.env.PORT),
+      'process.env.DEBUG': JSON.stringify(process.env.DEBUG),
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          cssnano({
+            autoprefixer: {
+              browsers: [
+                'safari 9',
+                'ie 10-11',
+                'last 2 Chrome versions',
+                'last 2 Firefox versions',
+                'edge 13',
+                'ios_saf 9.0-9.2',
+                'ie_mob 11',
+                'Android >= 4'
+              ],
+              cascade: false,
+              add: true,
+              remove: true
+            },
+            safe: true
+          })
+        ]
       }
     })
   ],
   resolve: {
-    modulesDirectories: ['node_modules', SRC],
-    extensions: ['', '.js', '.jsx', '.scss']
+    modules: ['node_modules', SRC],
+    extensions: ['.js', '.jsx', '.scss']
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         include: [/src/],
-        loader: 'babel'
+        loader: 'babel-loader'
       },
       {
         test: /\.scss$/,
         include: [/src/],
-        loader: ExtractTextPlugin.extract('style', [
-          'css?sourceMap',
-          'postcss',
-          'sass?sourceMap&outputStyle=expanded'])
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
       }
     ]
-  },
-  postcss: [
-    cssnano({
-      autoprefixer: {
-        browsers: [
-          'safari 9',
-          'ie 10-11',
-          'last 2 Chrome versions',
-          'last 2 Firefox versions',
-          'edge 13',
-          'ios_saf 9.0-9.2',
-          'ie_mob 11',
-          'Android >= 4'
-        ],
-        cascade: false,
-        add: true,
-        remove: true
-      },
-      safe: true
-    })
-  ]
+  }
 };
